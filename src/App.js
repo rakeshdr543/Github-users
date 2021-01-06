beefd1c382cf589ead91769e8c1d5fda2cfcb010
rakeshdr543 <rakeshdr543@gmail.com>
2021-01-06 06:26:53 +0530

    Added routes

--- src/App.js
+++ src/App.js
@@ -4,11 +4,15 @@ import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 function App() {
   return (
-    <div>
+    <Router>
+    <Switch>
+      <Route path='/' exact >
       <Dashboard></Dashboard>
-      <Login />
-      <Error />
-    </div>
+      </Route>
+      <Route path='/login'><Login /></Route>
+      <Route path='/login'> <Error /></Route>
+      </Switch>
+    </Router>
   );
 }
 
