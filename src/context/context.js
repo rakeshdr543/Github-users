e3ed83401fcdf28aabcfa4082c33c83229d8e2d5
rakeshdr543 <rakeshdr543@gmail.com>
2021-01-07 08:01:15 +0530

    Added request functions in context

--- src/context/context.js
+++ src/context/context.js
@@ -12,6 +12,41 @@ export const GithubProvider = ({children}) =>{
     const [githubUser,setGithunUser]=useState(mockUser)
     const [repos,setRepos]=useState(mockRepos)
     const [followers,setFollowers]=useState(mockFollowers)
+    // request loading
+    const [requests,setRequests]= useState(0);
+    const [isLoading,setIsLoading]=useState(false)
+    //error
+    const [error,setError]=useState({show:false,msg:''})
+
+    const searchGithubUser = async(user) =>{
+        toggleError()
+        setIsLoading(true)
+        const response = await axios(`${rootUrl}/users/${user}`).catch((err) =>
+        console.log(err)
+        )
+        if(response){
+            setGithunUser(response.data)
+            const { login, followers_url } = response.data;
+        }
+    }
+
+    const checkRequests = () =>{
+        axios(`${rootUrl}/rate_limit`)
+        .then(({data})=>{
+            let {
+                rate: { remaining },
+              } = data;
+              setRequests(remaining)
+              if(remaining === 0){
+                  toggleError(true,'sorry, you have exceeded your hourly rate limit!')
+              }
+        }).catch((err)=>console.log(err))
+    }
+    function toggleError(show=false,msg=''){
+        setError({show,msg})
+    }
+
+    useEffect(checkRequests, []);
 
     return(
         <GithubContext.Provider value={{githubUser,repos,followers}} >
