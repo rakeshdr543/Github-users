4a6bd335d2a9c4053177421173d5775c3206523b
rakeshdr543 <rakeshdr543@gmail.com>
2021-01-07 06:33:21 +0530

    Added fusion charts

--- src/components/Repos.js
+++ src/components/Repos.js
@@ -1,47 +1,62 @@
 import React from 'react';
 import styled from 'styled-components';
 import { GithubContext } from '../context/context';
-import { ExampleChart, Pie3D, Column3D, Bar3D, Doughnut2D } from './Charts';
+import { Pie, Column, Bar, Doughnut } from './Charts';
 const Repos = () => {
 
   const {repos} = React.useContext(GithubContext)
-console.log(repos)
-  let languages=repos.reduce((total,item)=>{
-    const {language} =item
+
+  const languages=repos.reduce((total,item)=>{
+    const {language, stargazers_count} =item
     if(!language) return total
     if(!total[language]){
-      total[language]={label:language,value:1}
+      total[language]={label:language,value:1,stars:stargazers_count}
     } else {
       total[language]={
         ...total[language],
-        value:total[language].value + 1
+        value:total[language].value + 1,
+        stars:total[language].stars + stargazers_count
       }
     }
     return total
   },{})
 
-  languages=Object.values(languages).sort((a,b)=>{
+  const mostUsed = Object.values(languages).sort((a,b)=>{
     return b.value - a.value
   }).slice(0,5)
 
-  console.log(languages)
+  // most stars per language
+  const mostPopular = Object.values(languages).sort((a,b)=>{
+    return b.stars -a.stars
+  }).map((item)=>{
+    return {...item,value:item.stars}
+  }).slice(0,5)
 
-  const chartData=[
-    {
-      "label": "Javascript",
-      "value": "290"
-    },
-    {
-      "label": "React",
-      "value": "260"
-    },
-    {
-      "label": "Vue",
-      "value": "28"
-    }
-  ]
+  // stars , forks
+
+  let {stars,forks} = repos.reduce((total,item)=>{
+    const { stargazers_count, name, forks } = item;
+    total.stars[stargazers_count] = {label:name, value:stargazers_count}
+    total.forks[forks]={label:name,value:forks}
+    return total
+  },
+  {
+    stars:{},
+    forks:{}
+  })
+
+  stars = Object.values(stars).slice(-5).reverse()
+  forks = Object.values(forks).slice(-5).reverse()
+
   return (
-    <ExampleChart data={chartData} />
+    <section className='section'>
+      <Wrapper className='section-center'>
+        <Pie data={mostUsed} />
+        <Column data={stars} />
+        <Doughnut data={mostPopular} />
+        <Bar data={forks} />
+      </Wrapper>
+    </section>
   )
 };
 
