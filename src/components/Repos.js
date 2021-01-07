4326f17b9e25edb548accad653ca3bef5cde6f78
rakeshdr543 <rakeshdr543@gmail.com>
2021-01-07 21:31:35 +0530

    Added loading error page

--- src/components/Repos.js
+++ src/components/Repos.js
@@ -6,48 +6,55 @@ const Repos = () => {
 
   const {repos} = React.useContext(GithubContext)
 
-  const languages=repos.reduce((total,item)=>{
-    const {language, stargazers_count} =item
-    if(!language) return total
-    if(!total[language]){
-      total[language]={label:language,value:1,stars:stargazers_count}
+  const languages = repos.reduce((total, item) => {
+    const { language, stargazers_count } = item;
+    if (!language) return total;
+    if (!total[language]) {
+      total[language] = { label: language, value: 1, stars: stargazers_count };
     } else {
-      total[language]={
+      total[language] = {
         ...total[language],
-        value:total[language].value + 1,
-        stars:total[language].stars + stargazers_count
-      }
+        value: total[language].value + 1,
+        stars: total[language].stars + stargazers_count,
+      };
     }
-    return total
-  },{})
+    return total;
+  }, {});
 
-  const mostUsed = Object.values(languages).sort((a,b)=>{
-    return b.value - a.value
-  }).slice(0,5)
+  const mostUsed = Object.values(languages)
+  .sort((a, b) => {
+    return b.value - a.value;
+  })
+  .slice(0, 5);
 
-  // most stars per language
-  const mostPopular = Object.values(languages).sort((a,b)=>{
-    return b.stars -a.stars
-  }).map((item)=>{
-    return {...item,value:item.stars}
-  }).slice(0,5)
+// most stars per language
 
-  // stars , forks
+const mostPopular = Object.values(languages)
+  .sort((a, b) => {
+    return b.stars - a.stars;
+  })
+  .map((item) => {
+    return { ...item, value: item.stars };
+  })
+  .slice(0, 5);
 
-  let {stars,forks} = repos.reduce((total,item)=>{
+// stars, forks
+
+let { stars, forks } = repos.reduce(
+  (total, item) => {
     const { stargazers_count, name, forks } = item;
-    total.stars[stargazers_count] = {label:name, value:stargazers_count}
-    total.forks[forks]={label:name,value:forks}
-    return total
+    total.stars[stargazers_count] = { label: name, value: stargazers_count };
+    total.forks[forks] = { label: name, value: forks };
+    return total;
   },
   {
-    stars:{},
-    forks:{}
-  })
+    stars: {},
+    forks: {},
+  }
+);
 
-  stars = Object.values(stars).slice(-5).reverse()
-  forks = Object.values(forks).slice(-5).reverse()
-
+stars = Object.values(stars).slice(-5).reverse();
+forks = Object.values(forks).slice(-5).reverse();
   return (
     <section className='section'>
       <Wrapper className='section-center'>
--- src/context/context.js
+++ src/context/context.js
@@ -9,7 +9,7 @@ const rootUrl = 'https://api.github.com';
 export const GithubContext=React.createContext();
 
 export const GithubProvider = ({children}) =>{
-    const [githubUser,setGithunUser]=useState(mockUser)
+    const [githubUser,setGithubUser]=useState(mockUser)
     const [repos,setRepos]=useState(mockRepos)
     const [followers,setFollowers]=useState(mockFollowers)
     // request loading
@@ -18,17 +18,37 @@ export const GithubProvider = ({children}) =>{
     //error
     const [error,setError]=useState({show:false,msg:''})
 
-    const searchGithubUser = async(user) =>{
-        toggleError()
-        setIsLoading(true)
-        const response = await axios(`${rootUrl}/users/${user}`).catch((err) =>
-        console.log(err)
-        )
-        if(response){
-            setGithunUser(response.data)
-            const { login, followers_url } = response.data;
-        }
+  const searchGithubUser = async (user) => {
+    toggleError();
+    setIsLoading(true);
+    const response = await axios(`${rootUrl}/users/${user}`).catch((err) =>
+      console.log(err)
+    );
+    if (response) {
+      setGithubUser(response.data);
+      const { login, followers_url } = response.data;
+
+      await Promise.allSettled([
+        axios(`${rootUrl}/users/${login}/repos?per_page=100`),
+        axios(`${followers_url}?per_page=100`),
+      ])
+        .then((results) => {
+          const [repos, followers] = results;
+          const status = 'fulfilled';
+          if (repos.status === status) {
+            setRepos(repos.value.data);
+          }
+          if (followers.status === status) {
+            setFollowers(followers.value.data);
+          }
+        })
+        .catch((err) => console.log(err));
+    } else {
+      toggleError(true, 'there is no user with that username');
     }
+    checkRequests();
+    setIsLoading(false);
+  };
 
     const checkRequests = () =>{
         axios(`${rootUrl}/rate_limit`)
@@ -49,7 +69,7 @@ export const GithubProvider = ({children}) =>{
     useEffect(checkRequests, []);
 
     return(
-        <GithubContext.Provider value={{githubUser,repos,followers}} >
+        <GithubContext.Provider value={{githubUser,repos,followers,requests,error,searchGithubUser,isLoading}} >
         {children}
         </GithubContext.Provider>
     )
